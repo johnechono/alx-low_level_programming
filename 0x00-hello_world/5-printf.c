de0bccb910d205180f94ad116dd67e71e63ee2d8
johnechono <johninalegwu22@gmail.com>
2023-02-16 07:05:42 -0800

    using printf instead of puts

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - using printf to do the same job as puts
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
