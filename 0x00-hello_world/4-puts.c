69d2ab35de67241cf273ecff062cb51230fb4354
johnechono <johninalegwu22@gmail.com>
2023-02-16 04:58:36 -0800

    a program that prints a sentence followed by a new line

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+/**
+ * main - A program that prints a line with put function
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
