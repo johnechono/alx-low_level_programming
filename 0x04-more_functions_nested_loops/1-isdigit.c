bc70146efd50cba5e7b1a3832f2d1bd43de7ea14
johnechono <johninalegwu22@gmail.com>
2023-02-23 04:26:04 -0800

    prints last digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,15 +1,17 @@
 #include "main.h"
+
 /**
- * _isdigit - function that checks for a digit
- * @c: parameter to be checked
- * Return: 1 if it is a digit and 0 if otherwise
+ * _isdigit - input
+ *
+ * Description: define if a character is a number
+ * @c: charater
+ * Return: 1 yes 0 no
  */
 
 int _isdigit(int c)
 {
-	if (c >= 0 && c <= 9)
-	{
+	if (c >= '0' && c <= '9')
 		return (1);
-	}
-	return (0);
+	else
+		return (0);
 }
