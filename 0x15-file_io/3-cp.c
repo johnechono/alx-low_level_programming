32a8aea78eab5c07b7ea683a3edc3b247817db86
johnechono <johninalegwu22@gmail.com>
2023-04-09 12:48:59 -0700

    a program that copies the content of a file to another file

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * error_file - the program checks if the files can be opened.
+ * @file_from: file_from.
+ * @file_to: file_to.
+ * @argv: arguments vector.
+ * Return: nothing
+ */
+
+void error_file(int file_from, int file_to, char *argv[])
+{
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+}
+
+/**
+ * main - program check the code
+ * @argc: number of arguments
+ * @argv: arguments vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int file_from, file_to, file_error;
+	ssize_t char_no, w;
+	char buf[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
+		exit(97);
+	}
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	error_file(file_from, file_to, argv);
+
+	char_no = 1024;
+	while (char_no == 1024)
+	{
+		char_no = read(file_from, buf, 1024);
+		if (char_no == -1)
+			error_file(-1, 0, argv);
+		w = write(file_to, buf, char_no);
+		if (w == -1)
+			error_file(0, -1, argv);
+	}
+	file_error = close(file_from);
+	if (file_error == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+	file_error = close(file_to);
+	if (file_error == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+	return (0);
+}
